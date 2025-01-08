511c08c4fb359df74bd057c3e17a7f62c9c9eb07
kenshiro0907 <kennethotchoumare09@gmail.com>
2025-01-08 02:04:20 +0100

    try to fix mobile device problem 2

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
@@ -9,6 +9,45 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SectionWrapper(Tech, "");*/
+
+import React, { useState, useEffect } from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  const [isScreenLarge, setIsScreenLarge] = useState(window.innerWidth > 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenLarge(window.innerWidth > 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Limiter la liste des technologies à 4 si l'écran est petit
+  const displayedTechnologies = isScreenLarge
+    ? technologies
+    : technologies.slice(0, 4);
+
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {displayedTechnologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
